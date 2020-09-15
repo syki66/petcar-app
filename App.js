101c508652bfcfc85036624e07a5967e94b6b3f0
syki66 <66syki@gmail.com>
2020-09-16 03:19:30 +0900

    enable setinterval

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Button, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
+
+const input_url = "http://192.168.123.21:8080/dc_motor/3";
+
+const option = {
+  method: 'GET'
+}
+
+function fetchInterval () {
+  
+  setInterval(() => fetch(input_url, option),30 );
+  clearInterval(fetchInterval);
+}
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Button style={styles.button} onPress={clearInterval(fetchInterval)}
+      title="stop please">
+
+
+      </Button>
+
+      <TouchableOpacity onPressIn={() => useEffect} onPressOut={clearInterval(fetchInterval())}>
+        <View style={styles.button}>
+          <Text>Touch Here</Text>
+        </View>
+      </TouchableOpacity>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +43,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: 'red',
+    width: '100%',
+    height: '50%'
+  }
 });
