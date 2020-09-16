d325649f85b1044c8b9a8a474695c91ed013a42f
syki66 <66syki@gmail.com>
2020-09-16 19:08:33 +0900

    add input field and make styles

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, Button, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableWithoutFeedback, TouchableOpacity, TextInput, SafeAreaView } from 'react-native';
 
 const input_url = "http://192.168.123.21:8080/dc_motor/0";
 
@@ -16,14 +16,19 @@ export default class App extends React.Component {
   intervalID = 0;
   
   state = {
-    motor: true
+    host: "0.0.0.0",
+    port: "8080",
+    
+    DCMotor: true,
+
+    
   }
 
 
   startFetchInterval = () => {
     //console.log("start")
     this.intervalID = setInterval(() => fetch(input_url, option), 50 );
-    this.setState({ motor: true });
+    this.setState({ DCMotor: true });
     
   }
 
@@ -31,12 +36,42 @@ export default class App extends React.Component {
   stopFetchInterval = () => {
     //console.log("stop");
     clearInterval(this.intervalID);
-    this.setState({ motor: false });
+    this.setState({ DCMotor: false });
   }
 
+  submitHostPort(){
+    alert(`http://${this.state.host}:${this.state.port}`);
+  }
 
   render(){
     return (
+      <SafeAreaView style={styles.container}>
+        <TextInput 
+          placeholder="HOST 입력" 
+          onChangeText={(text) => this.setState( {host: text})}
+          style={styles.input}
+        />
+        
+        <TextInput
+          placeholder="PORT 입력"
+          onChangeText={(text) => this.setState({ port: text })}
+          style={styles.input}
+        />
+
+
+        <TouchableOpacity 
+          onPress={() => { this.submitHostPort() }}
+          style={styles.button}
+        >
+          <Text>제출</Text>
+        </TouchableOpacity>
+
+      </SafeAreaView>
+
+
+
+
+      /*
       <View style={styles.container}>
   
  
@@ -48,6 +83,7 @@ export default class App extends React.Component {
         
         <StatusBar style="auto" />
       </View>
+      */
     );
   }
 
@@ -55,6 +91,32 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    justifyContent: 'center'
+  },
+
+  input: {
+    borderWidth: 2,
+    borderColor: '#B7D0E7',
+    margin: 20,
+    height: 50,
+    fontSize: 50,
+    textAlign: 'center'
+  },
+
+  button: {
+    margin: 20,
+    height: 50,
+    fontSize: 50,
+    color: 'green',
+    backgroundColor: 'green',
+    justifyContent: 'center'
+    
+  }
+  
+  /*
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -66,4 +128,5 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '50%'
   }
+  */
 });
