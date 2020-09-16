23976e7f2d153de5679fcce8bf0140838cecd1f6
syki66 <66syki@gmail.com>
2020-09-16 18:21:57 +0900

    complete motor live control func

--- App.js
+++ App.js
@@ -2,38 +2,56 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react';
 import { StyleSheet, Text, View, Button, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
 
-const input_url = "http://192.168.123.21:8080/dc_motor/3";
+const input_url = "http://192.168.123.21:8080/dc_motor/0";
 
 const option = {
   method: 'GET'
 }
 
-function fetchInterval () {
+
+
+
+export default class App extends React.Component {
+
+  intervalID = 0;
   
-  setInterval(() => fetch(input_url, option),30 );
-  clearInterval(fetchInterval);
-}
+  state = {
+    motor: true
+  }
 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
+  startFetchInterval = () => {
+    //console.log("start")
+    this.intervalID = setInterval(() => fetch(input_url, option), 50 );
+    this.setState({ motor: true });
+    
+  }
 
-      <Button style={styles.button} onPress={clearInterval(fetchInterval)}
-      title="stop please">
+
+  stopFetchInterval = () => {
+    //console.log("stop");
+    clearInterval(this.intervalID);
+    this.setState({ motor: false });
+  }
 
 
-      </Button>
+  render(){
+    return (
+      <View style={styles.container}>
+  
+ 
+        <TouchableOpacity onPressIn={this.startFetchInterval} onPressOut={this.stopFetchInterval}>
+          <View style={styles.button}>
+            <Text>Touch Here</Text>
+          </View>
+        </TouchableOpacity>
+        
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 
-      <TouchableOpacity onPressIn={() => useEffect} onPressOut={clearInterval(fetchInterval())}>
-        <View style={styles.button}>
-          <Text>Touch Here</Text>
-        </View>
-      </TouchableOpacity>
-      
-      <StatusBar style="auto" />
-    </View>
-  );
+
 }
 
 const styles = StyleSheet.create({
